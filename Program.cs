2ac43570d8ce877efff17c8d04e2c51db76801ad
Alex Buck <buckatech@gmail.com>
2018-12-25 04:59:53 -0700

    xPath target first child

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
 using System;
 
 namespace TestHAP
@@ -8,22 +9,15 @@ class Program
     static void Main(string[] args)
     {
         string Url = "http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet";
-
-        HtmlWeb web = new HtmlWeb();
-        HtmlAgilityPack.HtmlDocument doc = web.Load(Url);
-    // /html/body/table[4]/tbody/tr/td[1]/table/tbody/tr[1]/th/center/b
-        var SpanNodes = doc.DocumentNode.SelectNodes("//table");
-        // if (SpanNodes != null)
-        //     {
-        //         Console.WriteLine(SpanNodes.InnerText);
-        //     }
-        foreach (var node in SpanNodes)
-            {
-                if (node.NodeType == HtmlNodeType.Element)
-                {
-                    Console.WriteLine(node.InnerText);
-                }
-            }
+        var web = new HtmlWeb();
+        var document = web.Load(Url);
+        var c = document.DocumentNode.SelectSingleNode("//td[contains(text(),'Alberta Total Net Generation')]");
+        HtmlNode sibling = c.NextSibling;
+        // body > table:nth-child(9) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > th > center > b
+        if(c != null)
+        {
+            Console.WriteLine(sibling.InnerHtml);
+        }
     }
 }
 }
