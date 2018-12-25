f2db4059e035a6f356444936a89b8ab6eb03ff39
Alex Buck <buckatech@gmail.com>
2018-12-25 04:15:16 -0700

    initital commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace TestHAP
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        string Url = "http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet";
+
+        HtmlWeb web = new HtmlWeb();
+        HtmlAgilityPack.HtmlDocument doc = web.Load(Url);
+    // /html/body/table[4]/tbody/tr/td[1]/table/tbody/tr[1]/th/center/b
+        var SpanNodes = doc.DocumentNode.SelectNodes("//table");
+        // if (SpanNodes != null)
+        //     {
+        //         Console.WriteLine(SpanNodes.InnerText);
+        //     }
+        foreach (var node in SpanNodes)
+            {
+                if (node.NodeType == HtmlNodeType.Element)
+                {
+                    Console.WriteLine(node.InnerText);
+                }
+            }
+    }
+}
+}
