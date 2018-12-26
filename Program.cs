79fe0c3ab57eb55b89f278bde7b85a724875c8d4
Alex Buck <buckatech@gmail.com>
2018-12-26 01:14:24 -0700

    list all node elements

--- Program.cs
+++ Program.cs
@@ -1,23 +1,39 @@
-﻿using HtmlAgilityPack;
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
 using Fizzler.Systems.HtmlAgilityPack;
-using System;
-
-namespace TestHAP
+using System.Text.RegularExpressions;
+namespace MyApp
 {
-class Program
-{
-    static void Main(string[] args)
-    {
-        string Url = "http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet";
-        var web = new HtmlWeb();
-        var document = web.Load(Url);
-        var c = document.DocumentNode.SelectSingleNode("//td[contains(text(),'Alberta Total Net Generation')]");
-        HtmlNode sibling = c.NextSibling;
-        // body > table:nth-child(9) > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > th > center > b
-        if(c != null)
-        {
-            Console.WriteLine(sibling.InnerHtml);
+    public class Program {
+        public static void Main(string[] args) {
+            // Url to get Html from
+            var targetUrl = @"http://ets.aeso.ca/ets_web/ip/Market/Reports/CSDReportServlet";
+            
+            HtmlWeb web = new HtmlWeb();
+            // Instantiate a new generic list
+            List<HtmlNode> scrapeParts = new List<HtmlNode>();
+            
+            var htmlDoc = web.Load(targetUrl);
+            // Get the Body and all decendents from targetUrl
+            var bodysOfScrapeBody = htmlDoc.DocumentNode.QuerySelectorAll("body");
+            /* Add each body into scrapeParts
+                scrapeParts[0] Contains Scripts/Headers
+                scrapeParts[1] Contains the main data body
+            */
+            foreach (var bodyNode in bodysOfScrapeBody) {
+                if (bodyNode.NodeType == HtmlNodeType.Element) {
+                    scrapeParts.Add(bodyNode);
+                }
+            }
+            //var testNode = scrapeParts[1].QuerySelector("tr tr");
+            foreach (var nNode in scrapeParts[1].Descendants())
+                {
+                    if (nNode.NodeType == HtmlNodeType.Element)
+                    {
+                        Console.WriteLine(nNode.InnerHtml);
+                    }
+                }
         }
     }
-}
 }
