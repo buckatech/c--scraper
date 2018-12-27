e5a1448cbc80a361bf39173472a5c85cfbeb4666
Alex Buck <buckatech@gmail.com>
2018-12-26 23:37:36 -0700

    switch logs each bio element correctly

--- Program.cs
+++ Program.cs
@@ -9,11 +9,25 @@ namespace MyApp
     {
         public List<string> Title { get; set; }
         public List<HtmlNode> Tables { get; set; }
+        public List<HtmlNode> DataTable { get; set; }
         public TableData()
         {
             Title = new List<string>();
             Tables = new List<HtmlNode>();
+            DataTable = new List<HtmlNode>();
         }
+    }
+    public class FuelData
+    {
+        // Auto-implemented readonly property:
+        public string FuelTitle { get; set; }
+    }
+    public class McTngDcr
+    {
+        public string Asset { get; set; }
+        public string Mc { get; set; }
+        public string Tng { get; set; }
+        public string Dcr { get; set; }
     }
     public class Program {
         public static void Main(string[] args) 
@@ -68,7 +82,54 @@ namespace MyApp
             // Tables[13] Contains hydro table
             // Tables[14] Contains wind table
             // Tables[15] Contains biomass and other table
-            Console.WriteLine(mainTable.Tables[15].InnerHtml);
+            foreach (var i in mainTable.Tables[15].Descendants("tr"))
+            {
+                    if (i.NodeType == HtmlNodeType.Element)
+                    {
+                        mainTable.DataTable.Add(i);
+                    }           
+            }
+            //dt[0] title
+            //dt[1] css (bad)
+            //dt [2]+ is data for each plant
+            var bioTable = new FuelData();
+            bioTable.FuelTitle = mainTable.DataTable[0].InnerText;
+            mainTable.DataTable.RemoveRange(0, 2);
+            var mcList = new List<McTngDcr>();
+            var k = mainTable.DataTable;
+            for (int j = 0; j < k.Count; j++)
+            {
+                if (k[j].NodeType == HtmlNodeType.Element)
+                {
+                    // foreach (var item in j.ChildNodes)
+                    // {
+                    //     Console.WriteLine(item.InnerText);
+                    // }
+                    for (int i = 0; i < 4; i++)
+                    {
+                        switch (i)
+                        {
+                            case 0:
+
+                                Console.WriteLine(k[j].ChildNodes[i].InnerHtml + "case1");
+                                break;
+                            case 1:
+                                Console.WriteLine(k[j].ChildNodes[i].InnerHtml + "case2");
+                                break;
+                            case 2:
+                                Console.WriteLine(k[j].ChildNodes[i].InnerHtml + "case3");
+                                break;
+                            case 3:
+                                Console.WriteLine(k[j].ChildNodes[i].InnerHtml + "case4");
+                                break;  
+                            default:
+                                Console.WriteLine("ERR");
+                                break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(mcList[0].Asset);
         }
     }
 }
