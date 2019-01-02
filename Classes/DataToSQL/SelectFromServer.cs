2d0298fccc140eed7fd43a42656f390dde7cb40f
Alex Buck <buckatech@gmail.com>
2019-01-02 07:44:52 -0700

    Lift program to serverhandler

--- Classes/DataToSQL/SelectFromServer.cs
+++ Classes/DataToSQL/SelectFromServer.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-namespace myApp.Classes.DataToSQL
+using Npgsql;
+using NpgsqlTypes;
+using System.Collections.Generic;
+using ConnectSecret;
+
+namespace Scouter.Classes.DataToSql
 {
     public class SelectFromServer
     {
-        public SelectFromServer()
+        public void SelectMain()
         {
+            // Open the connection to the psqlDb
+            using (var conn = new NpgsqlConnection(ConnectToNpgSQL.GetConnection()))
+                try
+                {
+                    conn.Open();
+                    using (var cmd = new NpgsqlCommand("SELECT asset FROM coal_table WHERE main_table_id = 1", conn))
+                    using (var reader = cmd.ExecuteReader())
+                        while (reader.Read())
+                            Console.WriteLine(reader.GetString(0));
+
+                    conn.Close();
+                }
+                catch (NpgsqlException ex)
+                {
+                    Console.WriteLine(ex);
+                    long err = 0;
+                    Console.WriteLine(err);
+                }
+
         }
     }
 }
---
+++ Classes/ServerHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Scouter.Classes.MainData;
+using Scouter.Classes.DataToSql;
+using System.Collections.Generic;
+namespace Scouter.Classes
+{
+    public class ServerHandler
+    {
+        public void InsertToMainServer()
+        {
+            var Data = new GetData();
+            var coal = Data.CoalData;
+            var wind = Data.WindData;
+            var biomass = Data.BiomassData;
+            var hydro = Data.HydroData;
+            var summary = Data.SummaryData;
+            var gas = Data.GasData;
+            var interchange = Data.InterchangeData;
+            long currentTime = DateTime.Now.Ticks;
+            var db = new InsertToServer();
+            var selectDb = new SelectFromServer();
+            var util = new DataUtilities();
+            util.DbMigrate();
+            long pK = db.InsertMain("main_table", currentTime);
+            if (pK > 0)
+            {
+                Console.WriteLine(pK);
+            }
+            else
+            {
+                Console.WriteLine("FAIL FAIL FAIL");
+            }
+            foreach (KeyValuePair<string, List<string>> entry in coal)
+            {
+                db.InsertFuelRecord("coal_table", pK, entry.Key, entry.Value[0], entry.Value[1], entry.Value[2]);
+            }
+            foreach (KeyValuePair<string, List<string>> entry in wind)
+            {
+                db.InsertFuelRecord("wind_table", pK, entry.Key, entry.Value[0], entry.Value[1], entry.Value[2]);
+            }
+            foreach (KeyValuePair<string, List<string>> entry in hydro)
+            {
+                db.InsertFuelRecord("hydro_table", pK, entry.Key, entry.Value[0], entry.Value[1], entry.Value[2]);
+            }
+            foreach (KeyValuePair<string, List<string>> entry in biomass)
+            {
+                db.InsertFuelRecord("biomass_table", pK, entry.Key, entry.Value[0], entry.Value[1], entry.Value[2]);
+            }
+            foreach (KeyValuePair<string, string> entry in summary)
+            {
+                db.InsertSummary("summary_table", pK, entry.Key, entry.Value);
+            }
+            foreach (KeyValuePair<string, string> entry in interchange)
+            {
+                db.InsertSummary("interchange_table", pK, entry.Key, entry.Value);
+            }
+            for (var i = 0; i < gas.Count; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        foreach (KeyValuePair<string, List<string>> entry in gas[i])
+                        {
+                            db.InsertFuelRecord("simple_cycle_table", pK, entry.Key, entry.Value[0], entry.Value[1], entry.Value[2]);
+                        }
+                        break;
+                    case 1:
+                        foreach (KeyValuePair<string, List<string>> entry in gas[i])
+                        {
+                            db.InsertFuelRecord("cogeneration_table", pK, entry.Key, entry.Value[0], entry.Value[1], entry.Value[2]);
+                        }
+                        break;
+                    case 2:
+                        foreach (KeyValuePair<string, List<string>> entry in gas[i])
+                        {
+                            db.InsertFuelRecord("combined_cycle_table", pK, entry.Key, entry.Value[0], entry.Value[1], entry.Value[2]);
+                        }
+                        break;
+
+                }
+            }
+        }
+    }
+}
